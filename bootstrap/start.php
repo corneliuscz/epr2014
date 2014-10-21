4592972a00f090be3560014446bacedd94b7014d
Josef Vybíral <josef.vybiral@gmail.com>
2014-10-21 09:54:52 +0200

    Správně nastavení lokálního prostředí :-)

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-  'local' => array('vybiralpc'),
-  'local' => array('Tycho'),
+  'local' => array('vybiralpc','Tycho'),
   
   //'local' => array('homestead'),
 
