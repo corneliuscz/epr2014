23c807c2e296553ff697c3cb39066efd63fa0a75
Josef Vybíral <josef.vybiral@gmail.com>
2014-10-21 10:50:41 +0200

    Oprava CSS v administraci

--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <title>Administrace EPR 2014</title>
 
     <!-- Bootstrap -->
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
